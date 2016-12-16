3c20243e8d046a09c67a40a5625660b123db8493
Shoniwa <stmarovatsanga@gmail.com>
2016-12-16 20:53:01 +0000

    test all expectations

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,4 +9,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "fizz" when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
 end
